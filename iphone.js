d982228d88808cfcaac4b0118079513baf171915
Micke <mikael.larsen@student.kyh.se>
2012-02-02 09:38:19 +0100

    different profile pics now available

--- iphone.js
+++ iphone.js
@@ -1,3 +1,21 @@
 var jQT = $.jQTouch({
 	
 });
+
+var playerid;
+
+$(function(){
+	$('#playerlist li').click( function(e){
+		playerid = $(this).attr('id');
+		console.log(playerid);
+	});
+});
+
+$(function(){
+     $('#iconlist li a').click( function(e){
+        var icon = $(this).attr('id')+'.jpg';
+        
+        $('#'+playerid+' a img').attr('src', icon);
+        
+	  });
+});
