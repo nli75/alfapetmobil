47b5a1822a09b1f8616108930c54db33361c379d
Micke <mikael.larsen@student.kyh.se>
2012-02-02 11:25:38 +0100

    started db work, profilepictures now on main game screen

--- iphone.js
+++ iphone.js
@@ -2,11 +2,21 @@ var jQT = $.jQTouch({
 	
 });
 
+var databaseOptions = {
+	fileName : 'alfapet_database',
+	version : "1.0",
+	displayName : "alfapetDB",
+	maxSize : 1024
+}
+
+var database = openDatabase(databaseOptions.fileName, databaseOptions.version,
+							databaseOptions.displayName, databaseOptions.maxSize);
+
 var playerid;
 
 $(function(){
 	$('#playerlist li').click( function(e){
-		playerid = $(this).attr('id');
+		playerid = $(this).attr('id').slice(10);
 		console.log(playerid);
 	});
 });
@@ -15,7 +25,11 @@ $(function(){
      $('#iconlist li a').click( function(e){
         var icon = $(this).attr('id')+'.jpg';
         
-        $('#'+playerid+' a img').attr('src', icon);
-        
+        $('#playerlist'+playerid+' a img').attr('src', icon);
+        $('#icon_player'+playerid+' img').attr('src', icon);
 	  });
+});
+
+$(function(){
+	
 });
